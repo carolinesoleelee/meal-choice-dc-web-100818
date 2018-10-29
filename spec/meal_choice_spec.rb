0929cef14c25c009ede3ff4068f9088f997d3f7e
Solee Lee <carolinesoleelee@gmail.com>
2018-10-29 18:52:17 -0400

    Done.

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -3,10 +3,6 @@ describe '#meal_choice' do
     expect(meal_choice).to eq("meat")
   end
 
-def meal_choice(meal = "meat")
-  put = "#{meal}"
-end
-
   it 'should allow you to set a meal' do
     expect(meal_choice("vegan")).to eq("vegan")
   end
